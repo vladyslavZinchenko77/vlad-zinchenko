abdfced895c5d90a3c50fa3a73a659e35d599d1e
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2024-03-25 19:27:24 +0200

    added title component

--- src/components/ui/Experience/Experience.tsx
+++ src/components/ui/Experience/Experience.tsx
@@ -1,9 +1,11 @@
 import { FC } from 'react';
 import CustomizedTimeline from './CustomizedTimeline';
+import Title from '@/components/common/Title/Title';
 
 const Experience: FC = () => {
   return (
     <div>
+      <Title text={'Experience'} />
       <h2>Experience</h2> <CustomizedTimeline />
     </div>
   );
