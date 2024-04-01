94d5cf50446367718f5c5e00db8c4696b651712f
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2024-04-01 12:26:39 +0300

    refactored a component btn

--- src/components/common/Btn/Btn.tsx
+++ src/components/common/Btn/Btn.tsx
@@ -1,40 +1,75 @@
-import { FC, CSSProperties } from 'react';
+// import { FC, CSSProperties } from 'react';
+// import './Btn.scss';
+
+// interface BtnProps {
+//   onClick?: () => void;
+//   children: React.ReactNode;
+//   isLink: boolean;
+//   href?: string;
+//   styles?: CSSProperties;
+//   id?: string;
+// }
+
+// const Btn: FC<BtnProps> = ({ onClick, children, isLink, href, styles, id }) => {
+//   return (
+//     <>
+//       {isLink ? (
+//         <div id={id} className="on-light">
+//           <a
+//             style={styles}
+//             href={href}
+//             target="_blank"
+//             className="link border-gradient border-gradient-purple"
+//           >
+//             {children}
+//           </a>
+//         </div>
+//       ) : (
+//         <div id={id} className="on-light">
+//           <button
+//             style={styles}
+//             className="btn border-gradient border-gradient-purple"
+//           >
+//             {children}
+//           </button>
+//         </div>
+//       )}
+//     </>
+//   );
+// };
+
+// export default Btn;
+
+import { FC, CSSProperties, ReactNode } from 'react';
 import './Btn.scss';
 
 interface BtnProps {
   onClick?: () => void;
-  children: React.ReactNode;
+  children: ReactNode;
   isLink: boolean;
-  hRef?: string;
+  href?: string;
   styles?: CSSProperties;
-  id?: string;
+  id: string;
 }
 
-const Btn: FC<BtnProps> = ({ onClick, children, isLink, hRef, styles, id }) => {
+const Btn: FC<BtnProps> = ({ onClick, children, isLink, href, styles, id }) => {
+  const ButtonElement = isLink ? 'a' : 'button';
+
   return (
-    <>
-      {isLink ? (
-        <div id={id} className="on-light">
-          <a
-            style={styles}
-            href={hRef}
-            target="_blank"
-            className="link border-gradient border-gradient-purple"
-          >
-            {children}
-          </a>
-        </div>
-      ) : (
-        <div id={id} className="on-light">
-          <button
-            style={styles}
-            className="btn border-gradient border-gradient-purple"
-          >
-            {children}
-          </button>
-        </div>
-      )}
-    </>
+    <div className="on-light">
+      <ButtonElement
+        id={id}
+        style={styles}
+        href={isLink ? href : undefined}
+        target={isLink ? '_blank' : undefined}
+        onClick={onClick}
+        className={`btn border-gradient border-gradient-purple ${
+          isLink ? 'link' : ''
+        }`}
+      >
+        {children}
+      </ButtonElement>
+    </div>
   );
 };
 
